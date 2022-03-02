f5b9749a0a61a666210533084f10c6412ae02cf2
sasaharukimedes <sasaharu3@gmail.com>
2022-03-02 22:40:50 +0900

    I dont know why the link is not disappearing as it should

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,4 @@
 class NotificationsController < ApplicationController
-
   def index
     @notifications=current_user.passive_notifications
     #@notifications.where(checked: false).each do |notification|
