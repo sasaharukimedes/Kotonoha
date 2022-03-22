3ec492b4c06117288524e1d0d7d9179484956abf
sasaharukimedes <sasaharu3@gmail.com>
2022-03-22 23:26:39 +0900

    change and finish post&reply#show scss

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -18,12 +18,15 @@ class RepliesController < ApplicationController
     @reply = Reply.new(reply_params)
     @post = Post.find(params[:post_id])
     @reply.post_id = @post.id
-    @reply.save!
-
-    #通知メソッドの呼び出し
-    @reply.create_notification_by(current_user)
-    flash[:notice] = "返事が作られました!"
-    redirect_to home_path
+    if @reply.save
+      @reply.save!
+      #通知メソッドの呼び出し
+      @reply.create_notification_by(current_user)
+      flash[:notice] = "返事が作られました!"
+      redirect_to home_path
+    else
+      render "new"
+    end
 
     rescue ActiveRecord::RecordInvalid => e
       pp e.record.errors
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title='')
-    base_title = "言ノ葉"
+    base_title = "コトノハ"
     if page_title.empty?
       base_title
     else
